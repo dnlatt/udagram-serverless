872fc775c6f243de9f930cd439872a3effb81bdf
dnlatt <dnlatt@sph.com.sg>
2022-07-05 18:53:12 +0800

    Project init

---
+++ src/lambda/http/createGroup.ts
@@ -0,0 +1,38 @@
+import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda';
+import 'source-map-support/register';
+import * as AWS from 'aws-sdk';
+const uuid = require('uuid')
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const groupsTable = process.env.GROUPS_TABLE
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+
+    console.log('Processing event: ', event)
+  const itemId = uuid.v4()
+
+  const parsedBody = JSON.parse(event.body)
+
+  //const parsedBody = event
+
+  const newItem = {
+    id: itemId,
+    ...parsedBody
+  }
+
+  await docClient.put({
+    TableName: groupsTable,
+    Item: newItem
+  }).promise()
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      newItem
+    })
+  }
+}
